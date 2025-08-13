30c8b7c868ceca634010ee8d35046c880f75a89e
zoli0829 <zoli0829@gmail.com>
2025-08-13 19:03:58 +0100

    Initial commit

---
+++ ScrambledWords/ContentView.swift
@@ -0,0 +1,75 @@
+//
+//  ContentView.swift
+//  ScrambledWords
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var letters: [String] = ["O", "R", "A", "N", "G", "E"]
+    
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack {
+                Color.background
+                    .ignoresSafeArea()
+                
+                VStack {
+                    VStack {
+                        Spacer()
+                        
+                        Image("orange")
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                        
+                        Spacer()
+                        
+                        HStack {
+                            VStack {
+                                LetterView(character: "")
+                                Rectangle()
+                                    .fill(Color.white)
+                                    .frame(width: 25, height: 2)
+                            }
+                        }
+                        .padding(.bottom, 20)
+                    }
+                    .frame(width: proxy.size.width * 0.9, height: proxy.size.width * 0.9)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.border, lineWidth: 2)
+                    }
+                    
+                    Text("Score: 0")
+                        .font(.system(size: 15))
+                        .foregroundStyle(.white)
+                        .padding(.top)
+                    
+                    HStack {
+                        ForEach(letters, id: \.self) { letter in
+                            LetterView(character: letter)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+struct LetterView: View {
+    let character: String
+    
+    var body: some View {
+        Text(character)
+            .font(.system(size: 15, weight: .semibold))
+            .foregroundStyle(.white)
+            .frame(width:30, height: 30)
+            .background(Color.white.opacity(0.4))
+            .clipShape(RoundedRectangle(cornerRadius: 4))
+    }
+}
---
+++ ScrambledWords/ScrambledWordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambledWordsApp.swift
+//  ScrambledWords
+//
+//  Created by Gwinyai Nyatsoka on 10/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrambledWordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
